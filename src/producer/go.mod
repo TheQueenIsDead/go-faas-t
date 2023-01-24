f47e20b6a0c21e13983cf347e162031d1ccd3602
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2023-01-24 21:06:44 +1300

    :sparkles: feat: change producer to publish AMQP message with random image URI

--- src/producer/go.mod
+++ src/producer/go.mod
@@ -2,4 +2,7 @@ module producer
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.10.0
+require (
+	github.com/labstack/echo/v4 v4.10.0
+	github.com/rabbitmq/amqp091-go v1.6.0
+)
--- src/producer/producer.go
+++ src/producer/producer.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"io"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 const randomImageHost = "https://source.unsplash.com"
@@ -22,30 +25,66 @@ func imageHandler(c echo.Context) error {
 
 	// Send an HTTP GET request for the image
 	resp, err := http.Get(randomImageURL)
+	failOnError(err, fmt.Sprintf("Could not retrieve image from %s", randomImageURL))
 	if err != nil {
 		panic(err)
 	}
 
-	// Open a file to save the image
-	file, err := os.Create("image.jpeg")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	// Copy the image data from the response to the file
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
+	// Parse the resultant HTTP location after a 302 redirect to a random image
 	imageURI := fmt.Sprintf("%s%s", staticImageHost, resp.Request.URL.RequestURI())
 	parsedImageUri, err := url.PathUnescape(imageURI)
-	if err != nil {
-		panic(err)
-	}
+	failOnError(err, "Could not parse random image URI")
+
+	// Read RabbitMQ credentials from ENV.
+	// Named ✨ vaguely ✨ as we are just mounting the default credentials in kubernetes with envFrom.
+	amqpHost := os.Getenv("host")
+	amqpPort := os.Getenv("port")
+	amqpUser := os.Getenv("username")
+	amqpPass := os.Getenv("password")
+	amqpConnectionString := fmt.Sprintf("amqp://%s:%s@%s:%s/", amqpUser, amqpPass, amqpHost, amqpPort)
+
+	// Connect to AMQP and publish a message with the parsed image URI
+	conn, err := amqp.Dial(amqpConnectionString)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"media", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := parsedImageUri
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
 
 	return c.HTML(http.StatusOK, parsedImageUri)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
 }
 
 func main() {
